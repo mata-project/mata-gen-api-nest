fad1d9c1aa91826e506f3b4796efefde428e9bf1
cercinc <>
2024-12-11 13:10:20 +0100

    graphql first step

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { CatsModule } from './cats/cats.module';
+import { upperDirectiveTransformer } from './common/directives/upper-case.directive';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    CatsModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      typePaths: ['./**/*.graphql'],
+      transformSchema: schema => upperDirectiveTransformer(schema, 'upper'),
+      installSubscriptionHandlers: true,
+    }),
+  ],
 })
 export class AppModule {}
