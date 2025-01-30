a8af833a7b5a677aade1558f7a83dd05a7d63eaa
cercinc <>
2025-01-30 13:45:47 +0100

    cors added

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
 
+  app.enableCors({
+    origin: 'http://localhost:3000',
+  });
+
   await app.listen(3000, '0.0.0.0');
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
